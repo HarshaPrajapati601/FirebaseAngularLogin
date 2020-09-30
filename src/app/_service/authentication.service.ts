c4d202b93f5f45a6b6144895b4320c58655084c3
HarshaPrajapati <harsha_1605@yahoo.com>
2020-09-30 18:24:10 +0530

    login,register , logout logic

--- src/app/_service/authentication.service.ts
+++ src/app/_service/authentication.service.ts
@@ -1,15 +1,47 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable, Output } from '@angular/core';
+import { User } from 'firebase';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
+import { Users } from '../_models/User';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
-
-  constructor() { }
+  @Output() validationMessage = new EventEmitter();
+  private currentUserSubject : BehaviorSubject<Users>;
+  public currentUser : Observable<Users>;
+  registeredUsers: Users[] = [];
+  constructor() { 
+    this.currentUserSubject = new BehaviorSubject<Users>(JSON.parse(localStorage.getItem('current user')));
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
 
   //user Login
+  login(userName , password){
+    let userObj ={
+      userName : userName ,
+      password : password ,
+      email : "",
+      userid :""
+
+    }
+    //validate if user already there in Registeration array
+  }
 
   //user Logout
 
   //user Registration
+  register(userName , email , password){
+    let registeredStore ={
+      userName : userName ,
+      email :email ,
+      password : password,
+      userId : this.registeredUsers.length + 1
+    }
+  }
+  
+  //get current user
+  get currentUserValue() : Users{
+    return this.currentUserSubject.value;
+  }
 }

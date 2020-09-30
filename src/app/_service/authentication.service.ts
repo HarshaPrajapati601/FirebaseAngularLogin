6327f0cdd9e1fb153e6d047a017ca1c92f506251
HarshaPrajapati <harsha_1605@yahoo.com>
2020-09-30 17:00:52 +0530

    Basic structure creation with firebase setup

---
+++ src/app/_service/authentication.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  constructor() { }
+
+  //user Login
+
+  //user Logout
+
+  //user Registration
+}
